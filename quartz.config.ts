fae278c2df14b4b908f6efeba25c367952ba19d0
Kelno <kelnowm@gmail.com>
2024-12-25 22:15:16 +0100

    Quartz sync: Dec 25, 2024, 10:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-GB",
     baseUrl: "kelno.github.io/obsidian",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "attachments/!(public-|Public-)*.*",],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
